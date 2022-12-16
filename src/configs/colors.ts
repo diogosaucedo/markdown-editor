436e46080a97ecb3da53c6dc01570a180bd93bac
Diogo Ferreira Saucedo <Diogoferreiradfs@hotmail.com>
2022-12-16 16:25:00 -0400

    feat(colors): add color palette

--- src/configs/colors.ts
+++ src/configs/colors.ts
@@ -1,2 +1,7 @@
-export const primaryColor = '#000000';
-export const secondaryColor = '#FFFFFF';
+const black = '#0D0D0D';
+const darkGray = '#404040';
+const gray = '#595958';
+const lightGray = '#BFBAB0';
+const white = '#D9D6D0';
+
+export { black, darkGray, gray, lightGray, white };
