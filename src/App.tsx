a571193a82d2883e5c47ca2cd8a888abb2e136b9
Diogo Ferreira Saucedo <Diogoferreiradfs@hotmail.com>
2022-12-16 17:32:40 -0400

    feat: add markdown editor
    Construction of the logical part of the markdown editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,87 @@
 import React from 'react';
+import Button from './components/Form/Button';
+import Textarea from './components/Form/Textarea';
+import Center from './components/Layout/Center';
+import Flex from './components/Layout/Flex';
+import { black, white } from './configs/colors';
 import GlobalStyles from './styles/GlobalStyles';
+import hljs from 'highlight.js';
+import { marked } from 'marked';
+import View from './components/Editor/View';
+import JsPDF from 'jspdf';
+
+marked.setOptions({
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value;
+  },
+});
 
 function App() {
+  const [text, setText] = React.useState('');
+  const [md, setMd] = React.useState('');
+  const viewRef = React.useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  const handleChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    setText(e.currentTarget.value);
+    setMd(marked(text));
+  };
+
+  React.useEffect(() => {
+    setMd(marked(text));
+  }, [text]);
+
+  const toPDF = () => {
+    const doc = new JsPDF({
+      orientation: 'portrait',
+      format: 'a4',
+      unit: 'px',
+    });
+
+    doc.html(viewRef.current, {
+      async callback(doc) {
+        await doc.save('export.pdf');
+      },
+      x: 10,
+      y: 10,
+      width: 1000,
+    });
+  };
+
   return (
     <div className="App">
       <GlobalStyles />
-      <header>
-        <h1>Hello World!</h1>
-      </header>
+      <Center background={black} width="100%" height="48px">
+        <Button
+          onClick={toPDF}
+          background={white}
+          color={black}
+          width="96px"
+          height="100%"
+          fontSize="12px"
+          fontWeight={500}
+          borderRadius="4px"
+        >
+          Export PDF
+        </Button>
+      </Center>
+      <Flex width="100%">
+        <Textarea
+          width="50%"
+          minHeight="calc(100vh - 48px)"
+          value={text}
+          onChange={handleChange}
+          fontSize="16px"
+          padding="4px 8px"
+          placeholder="Type here"
+        ></Textarea>
+        <View
+          id="view"
+          width="50%"
+          height="100%"
+          dangerouslySetInnerHTML={{ __html: md }}
+          ref={viewRef}
+        ></View>
+      </Flex>
     </div>
   );
 }
